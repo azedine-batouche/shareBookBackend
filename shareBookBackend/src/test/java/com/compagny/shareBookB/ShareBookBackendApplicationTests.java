b362ef0bd93b998b911867d008e340178d21f237
AzedinePC <AzedinePC@Azedine>
2020-10-25 11:25:55 +0100

    init: initial project

---
+++ shareBookBackend/src/test/java/com/compagny/shareBookB/ShareBookBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compagny.shareBookB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareBookBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
